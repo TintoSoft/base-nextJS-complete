61ed5f183e55ee97ade55e88a22b6bc434b02557
TintoSoft <tintosoftdev@gmail.com>
2022-09-12 22:03:21 +0200

    first commit

--- src/components/atoms/Button/__tests__/Button.test.tsx
+++ src/components/atoms/Button/__tests__/Button.test.tsx
@@ -2,7 +2,7 @@ import { Button } from 'components/atoms/Button';
 import { render } from '@testing-library/react';
 
 describe('Button', () => {
-  it('render component', () => {
+  it('render components', () => {
     const { getByText } = render(<Button />);
 
     const button = getByText('Boton');
